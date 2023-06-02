881d683049740064688d084f316367dc487d3faf
Beatriz Lopes <bem.lopes978@gmail.com>
2023-06-02 09:15:58 +0100

    Burger Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*
 var anchor = document.querySelector ("#main-navigation").querySelectorAll("a");
 
 for(var i = 0; i < anchor.length; i++) {
@@ -5,3 +6,12 @@ for(var i = 0; i < anchor.length; i++) {
     anchor[i].classList.add("active");
     }
 }
+*/
+
+var btnToggleMenu = document.querySelector(".btn--toggle-menu");
+
+function toggleMenu() {
+    var mainNav = document.querySelector("#main-navigation");
+    mainNav.classList.toggle("menu-is-open");
+}
+btnToggleMenu.addEventListener("click", toggleMenu);
